33d93aeb56f2a16e3aa20956f92ca3afe6f23b0c
Akash <akash1608.n@gmail.com>
2025-06-05 23:59:11 +0530

    added the executable jar file into root

--- src/main/java/com/qa/utils/ConfigReader.java
+++ src/main/java/com/qa/utils/ConfigReader.java
@@ -9,13 +9,13 @@ public class ConfigReader {
     private static Properties prop;
 
     public static void intConfigReader() {
-        try {
-            prop = new Properties();
-            prop.load(new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\config\\qa_properties.properties"));
-            //"E:\\uiAutomation\\src\\test\\resources\\config\\qa_properties.properties"
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+//        try {
+//            prop = new Properties();
+//          // prop.load(new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\config\\qa_properties.properties"));
+//            //"E:\\uiAutomation\\src\\test\\resources\\config\\qa_properties.properties"
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
 
     }
 
